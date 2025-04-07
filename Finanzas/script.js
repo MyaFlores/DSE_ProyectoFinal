db5b7d5d9a859af28ab624ab11528317a742c085
git-oojl <jose.ortizosuna@cesunbc.edu.mx>
2025-04-06 21:16:24 -0700

    Add files via upload

---
+++ Finanzas/script.js
@@ -0,0 +1,8 @@
+// Función para el botón de regresar
+document.getElementById('backButton').addEventListener('click', () => {
+    window.history.back();
+  });
+  
+  // Si en el futuro se requiere funcionalidad para cada gráfico o cuadro, se pueden agregar eventos aquí
+  console.log("Dashboard prototipo cargado");
+  
